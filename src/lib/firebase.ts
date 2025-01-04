aee867286548e96d2a32642ca3d79060182d0602
Guilherme Viana <imluizguilara@hotmail.com>
2025-01-03 21:22:58 -0300

    chore: config firebase & env

---
+++ src/lib/firebase.ts
@@ -0,0 +1,28 @@
+import { env } from '@/utils/env'
+import { cert, getApps, initializeApp } from 'firebase-admin/app'
+import { getFirestore } from 'firebase-admin/firestore'
+import { getStorage } from 'firebase-admin/storage'
+import 'server-only'
+
+const decodedKey = Buffer.from(
+  env.FIREBASE_PRIVATE_KEY_BASE64,
+  'base64',
+).toString('utf-8')
+
+export const firebaseCert = cert({
+  projectId: env.FIREBASE_PROJECT_ID,
+  clientEmail: env.FIREBASE_CLIENT_EMAIL,
+  privateKey: decodedKey,
+})
+
+const hasInstanceAlready = Boolean(getApps().length)
+
+if (!hasInstanceAlready) {
+  initializeApp({
+    storageBucket: env.FIREBASE_STORAGE_BUCKET,
+    credential: firebaseCert,
+  })
+}
+
+export const db = getFirestore()
+export const storage = getStorage().bucket()
---
+++ src/utils/env.ts
@@ -0,0 +1,18 @@
+import { z } from 'zod'
+
+const envSchema = z.object({
+  FIREBASE_PROJECT_ID: z.string(),
+  FIREBASE_CLIENT_EMAIL: z.string(),
+  FIREBASE_PRIVATE_KEY_BASE64: z.string(),
+  FIREBASE_STORAGE_BUCKET: z.string(),
+})
+
+const parsedEnv = envSchema.safeParse(process.env)
+
+if (parsedEnv.success === false) {
+  console.error('❌ Invalid environment variables:', parsedEnv.error.format())
+
+  throw new Error('❌ Invalid environment variables!')
+}
+
+export const env = parsedEnv.data
